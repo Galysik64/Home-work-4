458ee38bd0e954f1a29bf53f4961e4743f632efc
Admin <galinapaskina61@gmail.com>
2024-10-12 21:08:32 +0400

    4work

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Задание 1");
+        int age = 19;
+        if (age >= 18) { System.out.println("Если возраст человека равен " + age + " он совершеннолетний");}
+        int ageTwoMen = 14;
+        if (ageTwoMen < 18) { System.out.println("Если возраст человека равен " + ageTwoMen + " он не достиг совершеннолетия, нужно немного подождать");}
+
+        System.out.println("Задание 2");
+
+    }
+}
